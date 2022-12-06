508057fde3facc2cda1387d9f0ad45eb0b269578
L-shuai <1472174772@qq.com>
2022-12-06 20:01:55 +0800

    组件生命周期函数

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -46,6 +46,31 @@
                 //参数2：传递的数据
                 this.$emit("onEvent",this.message)
             }
+        },
+        //生命周期函数
+        beforeCreate(){
+            console.log("beforeCreate:组件创建之前");
+        },
+        created(){
+            console.log("created:组件创建完成");
+        },
+        beforeMount(){
+            console.log("beforeMount：渲染之前");
+        },
+        mounted(){
+            console.log("mounted:组件渲染完成");
+        },
+        beforeUpdate(){
+            console.log("beforeUpdate:组件更新之前");
+        },
+        updated(){
+            console.log("updated:组件更新之后");
+        },
+        beforeUnmount(){
+            console.log("beforeUnmount:组件卸载之前");
+        },
+        unmounted(){
+            console.log("unmounted:组件卸载之后");
         }
     }
 </script>
